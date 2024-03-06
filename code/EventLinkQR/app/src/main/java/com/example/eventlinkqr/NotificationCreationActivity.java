f929168d5546f4e40f45325841d78af0a20f9139
Aryan Rusia <110370121+Aryan-Rusia-2@users.noreply.github.com>
2024-03-06 22:54:00 +0530

    Update NotificationCreationActivity.java

--- code/EventLinkQR/app/src/main/java/com/example/eventlinkqr/NotificationCreationActivity.java
+++ code/EventLinkQR/app/src/main/java/com/example/eventlinkqr/NotificationCreationActivity.java
@@ -33,7 +33,11 @@ public class NotificationCreationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notification);
 
-//
+// Temporarily commented out the notification sending functionality.
+// Initially created this page and added basic structure as a placeholder.
+// Currently testing notifications directly from the database. I will need to collaborate with Adrien for 
+// implementation of this feature, which I plan to address in the next PR. 
+        
 //        final EditText titleInput = findViewById(R.id.etNotificationTitle);
 //        final EditText messageInput = findViewById(R.id.etNotificationMessage);
 //        Button sendButton = findViewById(R.id.btnCreateNotification);
