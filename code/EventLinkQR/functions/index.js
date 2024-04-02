70b590a7865b97648a236f9a5f465eaf30cb2c51
Aryan-Rusia-2 <aryanrusia9602@gmail.com>
2024-04-03 02:39:32 +0530

    Removed bugs from cloud function and removed unnecessary code

--- code/EventLinkQR/functions/index.js
+++ code/EventLinkQR/functions/index.js
@@ -4,8 +4,17 @@ admin.initializeApp();
 
 exports.sendNotificationToEventAttendees = functions.firestore
     .document('/Notifications/{eventId}')
-    .onUpdate(async (change, context) => {
-        const notificationData = change.after.data(); // Get the state of the document after the change
+    .onWrite(async (change, context) => { // Changed from onUpdate to onWrite
+        // The 'change' parameter contains two properties: .before and .after
+        // You can check if the document was created by checking if 'change.before' exists
+        const documentWasCreated = !change.before.exists;
+        const notificationData = change.after.exists ? change.after.data() : null; // Check if the document still exists
+
+        if (!notificationData) {
+            console.log('Document was deleted');
+            return null;
+        }
+
         const eventId = context.params.eventId;
         let tokens = [];
 
@@ -15,114 +24,106 @@ exports.sendNotificationToEventAttendees = functions.firestore
         console.log('Updated notification for event ID:', eventId);
         console.log('The heading of the updated notification is:', lastNotification.heading);
         console.log('The description of the updated notification is:', lastNotification.description);
+        console.log('Is the notification a Milestone???? ', lastNotification.isMilestone);
 
-        try {
-            // Retrieve attendees UUIDs from the attendees subcollection of the Events collection
-            const attendeesSnapshot = await admin.firestore()
-                .collection('Events').doc(eventId)
-                .collection('attendees').get();
-
-            if (attendeesSnapshot.empty) {
-                console.log('No attendees for this event:', eventId);
-                return null; // Ensure to return null to terminate the function correctly
-            }
-
-            const attendeesTokensPromises = attendeesSnapshot.docs.map(async (attendeeDoc) => {
-                const userId = attendeeDoc.id; // UUID of the attendee
-                const userDoc = await admin.firestore().collection('Users').doc(userId).get();
-
-                if (userDoc.exists && userDoc.data().fcmToken) {
-                    const fcmToken = userDoc.data().fcmToken;
-                    console.log(`Token found for user ID: ${userId}`);
-                    tokens.push(fcmToken);
-
-                    // Update notifications in userNotifications collection for each user
-                    await admin.firestore().collection('userNotifications').doc(userId)
-                        .set({
-                            notifications: admin.firestore.FieldValue.arrayUnion({
-                                title: lastNotification.heading,
-                                body: lastNotification.description,
-                                eventId: eventId,
-                                timestamp: new Date(),
-                                isRead: false,
-                            })
-                        }, { merge: true });
-                } else {
-                    console.log(`No FCM token found for user ID: ${userId}`);
+        if (lastNotification.isMilestone) {
+            // Notify the event organizer instead of attendees
+            try {
+                const eventDoc = await admin.firestore().collection('Events').doc(eventId).get();
+                if (!eventDoc.exists) {
+                    console.log('Event document not found:', eventId);
+                    return null;
                 }
-            });
 
-            await Promise.all(attendeesTokensPromises);
-            console.log(`Notifications updated for ${tokens.length} attendees.`);
+                const organizerId = eventDoc.data().organizer;
+                const userDoc = await admin.firestore().collection('Users').doc(organizerId).get();
 
-            // Send updated notifications via FCM
-            const messages = tokens.map(token => ({
-                token,
-                notification: {
-                    title: lastNotification.heading,
-                    body: lastNotification.description,
+                if (!userDoc.exists || !userDoc.data().fcmToken) {
+                    console.log(`FCM token not found for organizer: ${organizerId}`);
+                    return null;
                 }
-            }));
 
-            const response = await admin.messaging().sendAll(messages);
-            console.log('Successfully sent messages:', response.successCount);
-        } catch (error) {
-            console.error('Error sending updated notifications:', error);
-        }
-    });
+                const fcmToken = userDoc.data().fcmToken;
+                // Assuming the notification to send is the last one added to the array
+                // const lastNotification = notificationData.notifications[notificationData.notifications.length - 1];
 
-
-    exports.addAdminRole = functions.https.onCall((data, context) => {
-        // check if request is made by an admin
-        if (context.auth.token.admin !== true) {
-            return { error: 'Only admins can add other admins, sucker' };
-        }
-    
-        // get user and add custom claim (admin)
-        return admin.auth().getUserByEmail(data.email)
-            .then(user => {
-                return admin.auth().setCustomUserClaims(user.uid, {
-                    admin: true
-                });
-            })
-            .then(() => {
-                return {
-                    message: `Success! ${data.email} has been made an admin.`
+                const message = {
+                    token: fcmToken,
+                    notification: {
+                        title: lastNotification.heading,
+                        body: lastNotification.description,
+                    }
                 };
-            })
-            .catch(err => {
-                return err;
-            });
+
+                await admin.messaging().send(message);
+                console.log(`Notification sent to the event organizer: ${organizerId}`);
+            } catch (error) {
+                console.error('Error sending notification to organizer:', error);
+                return null;
+            }
+        } else {
+            // Existing logic to notify event attendees...
+            // This is your current logic to handle notifications for attendees
+
+            try {
+                // Retrieve attendees UUIDs from the attendees subcollection of the Events collection
+                const attendeesSnapshot = await admin.firestore()
+                    .collection('Events').doc(eventId)
+                    .collection('attendees').get();
+
+                if (attendeesSnapshot.empty) {
+                    console.log('No attendees for this event:', eventId);
+                    return null; // Ensure to return null to terminate the function correctly
+                }
+
+                const attendeesTokensPromises = attendeesSnapshot.docs.map(async (attendeeDoc) => {
+                    const userId = attendeeDoc.id; // UUID of the attendee
+                    const userDoc = await admin.firestore().collection('Users').doc(userId).get();
+
+                    if (userDoc.exists && userDoc.data().fcmToken) {
+                        const fcmToken = userDoc.data().fcmToken;
+                        console.log(`Token found for user ID: ${userId}`);
+                        tokens.push(fcmToken);
+
+                        // Update notifications in userNotifications collection for each user
+                        await admin.firestore().collection('userNotifications').doc(userId)
+                            .set({
+                                notifications: admin.firestore.FieldValue.arrayUnion({
+                                    title: lastNotification.heading,
+                                    body: lastNotification.description,
+                                    eventId: eventId,
+                                    timestamp: new Date(),
+                                    isRead: false,
+                                })
+                            }, { merge: true });
+                    } else {
+                        console.log(`No FCM token found for user ID: ${userId}`);
+                    }
+                });
+
+                await Promise.all(attendeesTokensPromises);
+                console.log(`Notifications updated for ${tokens.length} attendees. [Not unique]`);
+
+                // New lines added to remove duplicate tokens:
+                const uniqueTokens = [...new Set(tokens)];
+                console.log(`Notifications updated for ${uniqueTokens.length} unique devices.`);
+
+
+                // Send updated notifications via FCM
+                const messages = uniqueTokens.map(token => ({
+                    token,
+                    notification: {
+                        title: lastNotification.heading,
+                        body: lastNotification.description,
+                    }
+                }));
+
+                const response = await admin.messaging().sendAll(messages);
+                console.log('Successfully sent messages:', response.successCount);
+            } catch (error) {
+                console.error('Error sending updated notifications:', error);
+            }
+        }
+
+
     });
-
-    
-// Helper function to generate a random code
-function generateRandomCode(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
-
-// Cloud Function to generate and store an admin code
-exports.generateAdminCode = functions.https.onCall((data, context) => {
-    // Optional: Check if the user requesting the code has permission
-    if (!context.auth || !context.auth.token.admin) { // Adjust based on your auth setup
-        throw new functions.https.HttpsError('permission-denied', 'Only admins can generate codes.');
-    }
-
-    const code = generateRandomCode(6); // Generate a 6-character code
-    const expiresAt = admin.firestore.Timestamp.fromDate(new Date(Date.now() + 24 * 3600 * 1000)); // 24 hours from now
-
-    // Store the code in Firestore
-    return admin.firestore().collection('AdminCodes').doc(code).set({
-        code,
-        expiresAt,
-    }).then(() => {
-        return { code, expiresAt };
-    }).catch(error => {
-        throw new functions.https.HttpsError('unknown', error.message, error);
-    });
-});
