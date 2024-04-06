213383363ffd11f758e6b8f4e20d268412b6e580
Aryan Rusia <110370121+Aryan-Rusia-2@users.noreply.github.com>
2024-04-06 22:16:14 +0530

    Removed unnecessary code from index.js

--- code/EventLinkQR/functions/index.js
+++ code/EventLinkQR/functions/index.js
@@ -4,9 +4,8 @@ admin.initializeApp();
 
 exports.sendNotificationToEventAttendees = functions.firestore
     .document('/Notifications/{eventId}')
-    .onWrite(async (change, context) => { // Changed from onUpdate to onWrite
-        // The 'change' parameter contains two properties: .before and .after
-        // You can check if the document was created by checking if 'change.before' exists
+    .onWrite(async (change, context) => {
+
         const documentWasCreated = !change.before.exists;
         const notificationData = change.after.exists ? change.after.data() : null; // Check if the document still exists
 
@@ -94,8 +93,6 @@ exports.sendNotificationToEventAttendees = functions.firestore
             }
         } 
         else {
-            // Existing logic to notify event attendees...
-            // This is your current logic to handle notifications for attendees
 
             try {
                 // Retrieve attendees UUIDs from the attendees subcollection of the Events collection
@@ -139,8 +136,6 @@ exports.sendNotificationToEventAttendees = functions.firestore
                 // New lines added to remove duplicate tokens:
                 const uniqueTokens = [...new Set(tokens)];
                 console.log(`Notifications updated for ${uniqueTokens.length} unique devices.`);
-
-
     
                 const eventDoc = await admin.firestore().collection('Events').doc(eventId).get();
                 if (!eventDoc.exists || !eventDoc.data().name) {
@@ -178,8 +173,6 @@ exports.sendNotificationToEventAttendees = functions.firestore
                 } catch (error) {
                     console.error('Error sending multicast messages:', error);
                 }
-                // const response = await admin.messaging().sendEachForMulticast(messages);
-                // console.log('Successfully sent messages:', response.successCount);
             } }
             catch (error) {
                 console.error('Error sending updated notifications:', error.error);
